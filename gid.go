8b29acfa0972a83ffea873ff5ffeab0a84724f85
theone <tanwei.rush@gmail.com>
2024-06-12 15:04:56 +0800

    1. 修改gid获取方式 2. 3. 4.

--- gid.go
+++ gid.go
@@ -1,10 +1,9 @@
 package logger
 
 import (
-	"bytes"
 	"log/slog"
-	"runtime"
-	"strconv"
+
+	"github.com/petermattis/goid"
 )
 
 type GIDContext struct {
@@ -37,10 +36,5 @@ func (g *GIDContext) Errorw() func(msg string, args ...any) {
 }
 
 func GetGID() uint64 {
-	b := make([]byte, 64)
-	b = b[:runtime.Stack(b, false)]
-	b = bytes.TrimPrefix(b, []byte("goroutine "))
-	b = b[:bytes.IndexByte(b, ' ')]
-	n, _ := strconv.ParseUint(string(b), 10, 64)
-	return n
+	return uint64(goid.Get())
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sunreaver/logger/v3
 go 1.21
 
 require (
+	github.com/petermattis/goid v0.0.0-20240607163614-bb94eb51e7a7
 	github.com/pkg/errors v0.9.1
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
